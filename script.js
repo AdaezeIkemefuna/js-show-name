f67dddaf8d2b5abf2b4b295ab86ca31acb84126a
Ada-ikemefuna <adaezeikemefuna@yahoo.com>
2022-06-21 14:56:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const myName = "Adaeze Ikemefuna";
+const height = '5"4';
+const country = "Nigeria";
+
+alert(`My name is Adaeze Ikemefuna
+My height is 5"4
+I am from Nigeria`);
+
+console.log(myName);
+console.log(height);
+console.log(country);
